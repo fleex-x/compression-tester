c8a140c1952e013a4775702944fbb294219eb454
TurovV <vladimir.turov.2002@gmail.com>
2022-04-10 15:05:58 +0300

    final

--- directory/collector.cpp
+++ directory/collector.cpp
@@ -12,14 +12,11 @@ public:
         if (!stat.verified) {
             std::cerr << "Non-verified compression" << std::endl;
         }
-        file[sourceName][funcName].push_back(
-                {"comp-time", std::to_string((double) stat.duration_comp_mics.count() / 1e6)});
-        file[sourceName][funcName].push_back(
-                {"decomp-time", std::to_string((double) stat.duration_decomp_mics.count() / 1e6)});
-        file[sourceName][funcName].push_back(
-                {"orig-size", std::to_string((double) stat.src_size / (1.0 * 1024 * 1024))});
-        file[sourceName][funcName].push_back({"comp-size", std::to_string(
-                (double) stat.compressed_size / (1.0 * 1024 * 1024))});
+        file[sourceName][funcName]["comp-time"] = std::to_string((double) stat.duration_comp_mics.count() / 1e6);
+        file[sourceName][funcName]["decomp-time"] = std::to_string((double) stat.duration_decomp_mics.count() / 1e6);
+        file[sourceName][funcName]["orig-size"] = std::to_string((double) stat.src_size / (1.0 * 1024 * 1024));
+        file[sourceName][funcName]["comp-size"] = std::to_string(
+                (double) stat.compressed_size / (1.0 * 1024 * 1024));
     }
 
     nlohmann::json get() {
