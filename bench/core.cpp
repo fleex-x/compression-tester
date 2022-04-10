b2dffc6adddc5a7d759f95bd0ccc81725fbc229f
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2022-04-10 12:42:23 +0300

    Bench first try

--- bench/core.cpp
+++ bench/core.cpp
@@ -3,18 +3,50 @@
 #include "timer.hpp"
 #include <iostream>
 
-Stat measure(std::string fname, CompressFunc compress, DecompressFunc decompress)
+Stat measure(std::string fname, CompressFunc compress, DecompressFunc decompress, int compr_level)
 {
+    FileHolder fh;
     try
     {
-        FileHolder fh(std::move(fname));
+        fh = FileHolder(std::move(fname));
     }
     catch (std::runtime_error &e)
     {
         std::cerr << e.what() << std::endl;
-        return {{}, {}, {}, false};
+        return {{}, {}, {}, {}, false};
     }
 
+    std::size_t size = fh.size();
 
+    const float coef = 1.5f;
+    std::vector<char> compressed(static_cast<std::size_t>(coef * size));
 
+    // compr
+    OneShotTimer timer_comp;
+    timer_comp.begin();
+
+    int comp_size = compress(fh.data(), compressed.data(), fh.size(), compressed.size(), compr_level);
+
+    timer_comp.end();
+
+    // decompr
+    std::vector<char> decompressed(static_cast<std::size_t>(size));
+
+    OneShotTimer timer_decomp;
+
+    int decomp_size = decompress(compressed.data(), decompressed.data(), comp_size, size);
+
+    timer_decomp.end();
+
+    // verify
+
+    bool ok = true;
+    ok &= static_cast<std::size_t>(decomp_size) == size;
+    if (ok) {
+        for (std::size_t  i = 0; i < size; ++i) {
+            ok &= decompressed[i] == fh.data()[i];
+        }
+    }
+
+    return Stat{timer_comp.mics(), timer_decomp.mics(), static_cast<int>(size), comp_size, ok};
 }
--- bench/core.hpp
+++ bench/core.hpp
@@ -3,15 +3,16 @@
 #include <string>
 
 using CompressFunc = int (*) (const char * src, char * dst, int src_size, int dst_size, int compression_level);
-using DecompressFunc = int (*) (const char * src, char * dst, int src_size, int dst_size, int compression_level);
+using DecompressFunc = int (*) (const char * src, char * dst, int src_size, int dst_size);
 
 
 // assume file size < 4GiB
 struct Stat {
-    std::chrono::microseconds duration_mics;
+    std::chrono::microseconds duration_comp_mics;
+    std::chrono::microseconds duration_decomp_mics;
     int src_size;
     int compressed_size;
     bool verified;
 };
 
-Stat measure(std::string fname, CompressFunc compress, DecompressFunc decompress);
+Stat measure(std::string fname, CompressFunc compress, DecompressFunc decompress, int compr_level);
